ac14640db988da0303e557c80742dd970cec0d6c
Mikhail <3124667+VasilievMB@users.noreply.github.com>
2022-12-02 10:06:34 +0100

    Update UILabel+Binding.swift

--- Sources/CombineUIKitBinding/UIKit Extensions/UILabel+Binding.swift
+++ Sources/CombineUIKitBinding/UIKit Extensions/UILabel+Binding.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  UILabel+Binding.swift
 //  
 //
 //  Created by Mikhail on 24.11.2022.
@@ -7,11 +7,33 @@
 
 import UIKit
 
-extension UILabel: BindingTarget {}
-
 public extension Binders where Target: UILabel {
     
     var text: Binder<String?> {
         WeakBinder(target: target) { label, text in label.text = text }
+    }
+    
+    var attributedText: Binder<NSAttributedString?> {
+        WeakBinder(target: target) { label, attributedText in label.attributedText = attributedText }
+    }
+    
+    var font: Binder<UIFont> {
+        WeakBinder(target: target) { label, font in label.font = font }
+    }
+    
+    var textColor: Binder<UIColor> {
+        WeakBinder(target: target) { label, textColor in label.textColor = textColor }
+    }
+    
+    var highlightedTextColor: Binder<UIColor?> {
+        WeakBinder(target: target) { label, highlightedTextColor in label.highlightedTextColor = highlightedTextColor }
+    }
+    
+    var isHighlighted: Binder<Bool> {
+        WeakBinder(target: target) { label, isHighlighted in label.isHighlighted = isHighlighted }
+    }
+    
+    var isEnabled: Binder<Bool> {
+        WeakBinder(target: target) { label, isEnabled in label.isEnabled = isEnabled }
     }
 }
